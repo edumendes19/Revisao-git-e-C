eac9133de332c753a5f31aee9149cb24a9b3cdf4
edumendes19 <em6608428@gmail.com>
2025-03-14 00:13:46 -0300

    Sexto Exercicio

---
+++ Exercicio6.c
@@ -0,0 +1,31 @@
+//a) Saída do programa:
+
+//while: 0
+//while: 1
+//while: 2
+//while: 3
+//while: 4
+//while: 5
+//while: 6
+//while: 7
+//while: 8
+//while: 9
+//do: 10
+
+//b) Diferença entre while e do...while:
+
+//while: Verifica a condição antes de executar o bloco. Se a condição for falsa na primeira vez, o código não é executado.
+//do...while: Executa o bloco de código pelo menos uma vez, verificando a condição depois.
+
+//c) Implementação do bloco:
+
+//Ambos os loops têm um bloco de código que imprime o valor de i e o incrementa.
+
+//d) Diferença entre while e for:
+
+//while: Usado quando o número de iterações não é conhecido antecipadamente.
+//for: Usado quando o número de iterações é conhecido e fixo.
+//Quando usar?
+
+//Use for quando souber o número exato de iterações.
+//Use while quando o número de iterações depender de uma condição variável.
