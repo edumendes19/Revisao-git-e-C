e1c3ba3e25f045e64c25ee20f3c32508da89a120
edumendes19 <em6608428@gmail.com>
2025-03-14 00:07:14 -0300

    Exercicio 5.c

---
+++ Exercicio 5.c
@@ -0,0 +1,25 @@
+//O que é um vetor?
+
+//Explique o funcionamento de um vetor e como ele é tratado no C
+//Explique o que uma string e como ela ´e tratada em C.
+
+
+
+//Vetor:
+
+//É uma sequência de elementos do mesmo tipo, acessados por índices que começam em 0.
+//Em C, é tratado como um ponteiro, e a memória é alocada de forma contígua.
+
+//Exemplo em C:
+
+//int vetor[5];  // Vetor de 5 inteiros
+//vetor[0] = 10;
+
+//2. String:
+
+//É um vetor de caracteres (char) em C, terminada com o caractere nulo '\0', que indica o fim da sequência.
+
+//Exemplo em C:
+
+//char nome[] = "Maria";  // String "Maria"
+//printf("%s", nome);     // Exibe "Maria"
