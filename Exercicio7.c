83e7177ebce737ad51e6b9b4625959cc48131083
edumendes19 <em6608428@gmail.com>
2025-03-14 00:17:53 -0300

    Sétimo Exercicio

---
+++ Exercicio7.c
@@ -0,0 +1,16 @@
+//a) Modos de Abertura de Arquivos:
+
+//"r": Lê arquivo existente.
+//"w": Escreve, sobrescrevendo se existir.
+//"a": Adiciona ao final do arquivo.
+//"r+": Lê e escreve em arquivo existente.
+//"w+": Lê e escreve, sobrescreve se existir.
+//"a+": Lê e escreve, adiciona ao final.
+//"b": Para arquivos binários.
+
+//b) Funções de Manipulação de Arquivos:
+
+//fgets(): Lê uma linha de um arquivo.
+//fprintf(): Escreve dados formatados em um arquivo.
+//fread(): Lê dados binários de um arquivo para um buffer.
+//fwrite(): Escreve dados binários de um buffer para um arquivo.
